19b51adf1032f921328ad5aad54d3ad202318bd9
Dheeraj1771 <dheerajkesavan@gmail.com>
2024-10-04 20:00:22 +0530

    day2

---
+++ Day-2/day2.java
@@ -0,0 +1,12 @@
+public class day2 {
+    public static int[] getConcatenation(int[] nums) {
+        int result[] = new int[nums.length * 2];
+        System.arraycopy(nums, 0, result, 0, nums.length);
+        System.arraycopy(nums, 0, result, nums.length, nums.length);
+        return result;
+    }
+    public static void main(String[] args) {
+        int nums[] = new int[] {1,2,3};
+        getConcatenation(nums);
+    }
+}
