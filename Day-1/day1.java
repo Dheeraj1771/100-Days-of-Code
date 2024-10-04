e89a85352006bdb1e59e4867121a38b009af5205
Dheeraj1771 <dheerajkesavan@gmail.com>
2024-10-04 19:50:47 +0530

    code

---
+++ Day-1/day1.java
@@ -0,0 +1,20 @@
+//Day1/100: Leetcode: 217. Contains Duplicate
+import java.util.*;
+class day1 {
+    public static boolean containsDuplicate(int[] nums) {
+        //Sorting 
+        Arrays.sort(nums);
+        int n = nums.length;
+        for(int i=1; i<n; i++) {
+            if(nums[i] == nums[i-1]) {
+                return true;
+            }
+        }
+        return false;   
+    }
+    public static void main(String[] args) {
+        int nums[] = new int[] {1,2,3,2,4,5};
+        containsDuplicate(nums);
+        
+    }
+}
