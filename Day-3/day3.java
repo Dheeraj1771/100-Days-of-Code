c990ae8cba9855e9bfa289a936d3ec17fcf2ac21
Dheeraj1771 <dheerajkesavan@gmail.com>
2024-10-05 10:46:30 +0530

    day3

---
+++ Day-3/day3.java
@@ -0,0 +1,32 @@
+////Day3/100: Leetcode: 9. Palindrome Number
+public class day3 {
+    class Solution {
+        public static boolean isPalindrome(int x) {
+            int temp = x;
+            int rem = 0;
+            int rev = 0;
+            if (x == 0) {
+                return true;
+            }
+            if (x < 0) {
+                return false;
+            }
+            else {
+                while(x > 0) {
+                    rem = x % 10;
+                    rev = (rev * 10) + rem;
+                    x = x/10; 
+                }
+                if (temp == rev) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }
+        public static void main(String[] args) {
+            boolean ans = isPalindrome(0);
+            System.out.println(ans);
+        }
+    }
+}
