1333ab975175eaccf06de2e88903caccf657078a
Dheeraj1771 <dheerajkesavan@gmail.com>
2024-10-06 16:44:36 +0530

    day4

---
+++ Day-4/day4.java
@@ -0,0 +1,21 @@
+//Day3/100: Leetcode: 7. Reverse Integer
+public class day4 {
+    public static int reverse(int x) {
+        int rev = 0;
+        int rem = 0;
+        while(x != 0) {
+            rem = x % 10;
+            if(rev > Integer.MAX_VALUE/10 || rev < Integer.MIN_VALUE/10) {
+                return 0;
+            } else {
+                rev = (rev * 10) + rem;
+                x = x/10;
+            }
+        }
+        return rev;
+    }
+    public static void main(String[] args) {
+        int ans = reverse(122);
+        System.out.println(ans);
+    }
+}
