a541552d460e39aaaca6b75381e0a824a0a55c6f
Dheeraj1771 <dheerajkesavan@gmail.com>
2024-10-07 22:22:01 +0530

    day5

---
+++ Day-5/day5.java
@@ -0,0 +1,22 @@
+//Day5/100: Leetcode: 1. Two Sum 
+import java.util.*;
+public class day5 {
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numMap = new HashMap<>();
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            int complement = target - nums[i];
+            if (numMap.containsKey(complement)) {
+                return new int[]{numMap.get(complement), i};
+            }
+            numMap.put(nums[i], i);
+        }
+        return new int[]{};
+    }
+    public static void main(String[] args) {
+        int nums[] = new int[] {1,2,3,4,5,6,7};
+        int ans[] = twoSum(nums, 9);
+        System.out.println(ans);
+        
+    }
+}
