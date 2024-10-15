2030d63a59ba589a68efc84ba18790bc10f10f8b
Dheeraj1771 <dheerajkesavan@gmail.com>
2024-10-15 11:32:26 +0530

    day13

---
+++ Day-13/day13.java
@@ -0,0 +1,25 @@
+
+public class day13 {
+    public static int mySqrt(int x) {
+        int low = 0;  
+        int high = x; 
+        if (x == 0 || x == 1) {
+            return x;
+        }
+        while (low <= high) {
+            int mid = low + (high - low) / 2;  
+            long val = (long) mid * mid;  
+            if (val == x) {
+                return mid;  
+            } else if (val < x) {
+                low = mid + 1;  
+            } else {
+                high = mid - 1;  
+            }
+        }
+        return high;
+    }
+    public static void main(String[] args) {
+        
+    }
+}
