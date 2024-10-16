11d8c98a46178febcee7541a69158a6ea41b2fb3
Dheeraj1771 <dheerajkesavan@gmail.com>
2024-10-16 10:59:54 +0530

    day14

---
+++ Day-14/day14.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class day14 {
+    public static List<Integer> selfDividingNumbers(int left, int right) {
+        ArrayList<Integer> list = new ArrayList<>();
+        int count = 0;
+        while(left <= right) {
+            int temp = left;
+            while(temp > 0) {
+                int digit = temp % 10;
+                if(digit == 0 || left % digit != 0) {
+                    count ++;
+                    break;
+                } else {
+                    temp = temp / 10;
+                }
+            }
+            if(count == 0) {
+                list.add(left);
+            } else {
+                count = 0;
+            }
+            left++;
+        }
+        return list;
+    }
+    public static void main(String[] args) {
+        int left = 1;
+        int right = 22;
+        List<Integer> ans = new ArrayList<Integer>();
+        ans = selfDividingNumbers(left, right);
+        System.out.println(ans);
+    }
+}
