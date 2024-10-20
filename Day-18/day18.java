477239a68cda97511b4903a187230c40a94ad020
Dheeraj1771 <dheerajkesavan@gmail.com>
2024-10-20 22:06:57 +0530

    day18

---
+++ Day-18/day18.java
@@ -0,0 +1,24 @@
+public class day18{
+    public static boolean isUgly(int n) {
+        if(n==0) {
+            return false;
+        } 
+        if(n > 0 && n <= 3) {
+            return true;
+        }
+        if(n % 2 == 0) {
+            return isUgly(n/2);
+        }
+        if(n % 3 == 0) {
+            return isUgly(n/3);
+        }
+        if(n % 5 == 0) {
+            return isUgly(n/5);
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        int n = 6;
+        isUgly(n);
+    }
+}
