c451fad425ec3d107e7bb4964fdde59ee74ce6a6
Dheeraj1771 <dheerajkesavan@gmail.com>
2024-10-22 22:58:59 +0530

    day20

---
+++ Day-20/day20.java
@@ -0,0 +1,8 @@
+//Day20/100: LeetCode: 367. Valid Perfect Square
+import java.util.Arrays;
+public class day20 {
+    public int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        return nums[nums.length/2];
+    }
+}
