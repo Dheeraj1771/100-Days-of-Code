49bd0ef5fb4262be8f298a04cf68c92138a73860
Dheeraj1771 <dheerajkesavan@gmail.com>
2024-10-27 18:05:18 +0530

    day25

---
+++ Day-25/day25.java
@@ -0,0 +1,23 @@
+//Day25/100: LeetCode: 202. Happy Number
+import java.util.*;
+public class day25 {
+    public boolean isHappy(int n) {
+        Set<Integer> hashSet = new HashSet<>();
+        while (true) {
+            int sum = 0;
+            while(n != 0) {
+                sum += Math.pow(n % 10, 2);
+                n = n/10;
+            }
+            if(sum == 1) {
+                return true;
+            }
+            n = sum;
+            if(hashSet.contains(n)) {
+                return false;
+            }
+            hashSet.add(n);
+        }
+    }
+    
+}
