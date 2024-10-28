7b3b1219e055a76fd3a1e82271eaeeee1b968f81
Dheeraj1771 <dheerajkesavan@gmail.com>
2024-10-28 13:49:18 +0530

    day26

--- Day-26/day26.java
+++ Day-26/day26.java
@@ -1,10 +1,15 @@
 //Day26/100: LeetCode: 136. Single Number
 public class day26 {
-    public int singleNumber(int[] nums) {
+    public static int singleNumber(int[] nums) {
         int ans = 0;
         for(int i=0; i<nums.length; i++) {
             ans ^= nums[i];
         }
         return ans;
+    }
+    public static void main(String[] args) {
+        int nums[] = new int[] {4,1,2,1,2};
+        int ans = singleNumber(nums);
+        
     }
 }
