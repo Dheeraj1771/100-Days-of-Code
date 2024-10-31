9c52edca3d1ba147b0cb7aae211c7a1b4cbb73ac
Dheeraj1771 <dheerajkesavan@gmail.com>
2024-10-31 23:56:22 +0530

    day29

--- Day-29/day29.java
+++ Day-29/day29.java
@@ -6,6 +6,8 @@ public class day29 {
         return Math.max(nums[0] * nums[1] * nums[n-1], nums[n-3] * nums[n-2] * nums[n-1]);
     }
     public static void main(String[] args) {
+        int nums[] = new int[] {-1,-2,-3};
         int ans = maximumProduct(nums);
+        System.out.println(ans);
     }
 }
