40fe531396d53baae6c51f24a5ab0c23cc154504
Dheeraj1771 <dheerajkesavan@gmail.com>
2024-11-01 22:31:54 +0530

    day30

---
+++ Day-30/day30.java
@@ -0,0 +1,14 @@
+public class day30 {
+    public boolean isPowerOfTwo(int n) {
+        if(n <= 0) {
+            return false;
+        }
+        if(n == 1) {
+            return true;
+        }
+        if(n % 2 == 0) {
+            return isPowerOfTwo(n/2);
+        }
+        return false;
+    }
+}
