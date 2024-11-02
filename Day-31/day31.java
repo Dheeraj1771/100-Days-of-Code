8160c6a059b16be58d1873e52fe34f1f38510654
Dheeraj1771 <dheerajkesavan@gmail.com>
2024-11-02 23:24:56 +0530

    day31

--- Day-31/day31.java
+++ Day-31/day31.java
@@ -1,10 +1,20 @@
 //Day31/100: LeetCode: 342. 338. Counting Bits
 public class day31 {
-    public int[] countBits(int n) {
+    public static int[] countBits(int n) {
         int ans[] = new int[n + 1];
         for (int i = 1; i <= n; i++) {
             ans[i] = ans[i >> 1] + (i & 1);
         }
         return ans;
+    }
+    public static void main(String[] args) {
+        int n = 12;
+        int ans[] = new int[n + 1];
+        ans = countBits(n);
+        System.out.println("[ ");
+        for(int i=0; i<ans.length; i++) {
+            System.out.println(ans[i] + " ");
+        }
+        System.out.println("]");
     }
 }
