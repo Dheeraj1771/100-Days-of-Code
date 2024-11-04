76a39484d592e5658992596e7d9877b06021eee3
Dheeraj1771 <dheerajkesavan@gmail.com>
2024-11-04 22:29:51 +0530

    day33

--- Day-33/day33.java
+++ Day-33/day33.java
@@ -15,6 +15,8 @@ public class day33 {
         return pq.peek();
     }
     public static void main(String[] args) {
+        int nums[] = new int[] {3,2,1,5,6,4};
+        int k = 2;
         int ans = findKthLargest(nums, k);
     }
 }
