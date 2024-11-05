31e17f724d4793e03c8e39b3329e2f07df9022d8
Dheeraj1771 <dheerajkesavan@gmail.com>
2024-11-05 09:16:30 +0530

    day34

--- Day-34/day34.java
+++ Day-34/day34.java
@@ -1,6 +1,6 @@
 //Day34/100: LeetCode: 292. Nim Game
 public class day34 {
-    public boolean canWinNim(int n) {
+    public static boolean canWinNim(int n) {
         return (n % 4 != 0);
     }
     public static void main(String[] args) {
