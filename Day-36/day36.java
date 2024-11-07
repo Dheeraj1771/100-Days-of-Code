57ab62cb509845db7390e21d95eca9d49a636399
Dheeraj1771 <dheerajkesavan@gmail.com>
2024-11-07 21:20:05 +0530

    day36

--- Day-36/day36.java
+++ Day-36/day36.java
@@ -1,7 +1,7 @@
 //Day36/100: LeetCode: 561. Array Partition
 import java.util.*;
 public class day36 {
-    public int arrayPairSum(int[] nums) {
+    public static int arrayPairSum(int[] nums) {
         Arrays.sort(nums);
         int maxsum = 0;
         for(int i=0; i<nums.length; i+=2) {
@@ -11,5 +11,6 @@ public class day36 {
     }
     public static void main(String[] args) {
         int nums[] = new int[] {6,2,6,5,1,2};
+        int ans = arrayPairSum(nums);
     }
 }
