0d34026342ee4469d0883f2bdd06a712c886c76b
Dheeraj1771 <dheerajkesavan@gmail.com>
2024-11-08 22:38:51 +0530

    day37

---
+++ Day-37/day37.java
@@ -0,0 +1,16 @@
+public class day37 {
+    public int maxSubArray(int[] nums) {
+        int maxsum = Integer.MIN_VALUE;
+        int currsum = 0;
+        for(int i=0; i<nums.length; i++) {
+            currsum = currsum + nums[i];
+            if(currsum > maxsum) {
+                maxsum = currsum;
+            }
+            if(currsum < 0) {
+                currsum = 0;
+            }
+        }
+        return maxsum;
+    }
+}
