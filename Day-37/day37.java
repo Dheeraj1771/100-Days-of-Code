91ef1c7a176ab8b0dd4d1d49068c2982f1e92471
Dheeraj1771 <dheerajkesavan@gmail.com>
2024-11-08 22:40:36 +0530

    day37

--- Day-37/day37.java
+++ Day-37/day37.java
@@ -16,6 +16,8 @@ public class day37 {
     }
     public static void main(String[] args) {
         int nums[] = new int[] {-2,1,-3,4,-1,2,1,-5,4};
+        int ans = maxSubArray(nums);
+        System.out.println(ans);
 
     }
 }
