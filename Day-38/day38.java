799ed6dd28fb572aff7ef999507335d777e07d06
Dheeraj1771 <dheerajkesavan@gmail.com>
2024-11-09 22:15:18 +0530

    day38

---
+++ Day-38/day38.java
@@ -0,0 +1,20 @@
+public class day38 {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        HashSet<Integer> set1 = new HashSet<>();
+        HashSet<Integer> intersect = new HashSet<>();
+        for(int i=0; i<nums1.length; i++) {
+            set1.add(nums1[i]);
+        }
+        for(int j=0;j<nums2.length; j++) {
+            if(set1.contains(nums2[j])) {
+                intersect.add(nums2[j]);
+            }
+        }
+        int res[] = new int[intersect.size()];
+        int index = 0;
+        for(int k : intersect) {
+            res[index++] = k;
+        }
+        return res;
+    }
+}
