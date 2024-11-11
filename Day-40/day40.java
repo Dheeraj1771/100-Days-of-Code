c182a8a1e72464319803f8a2d2de2020db36ea24
Dheeraj1771 <dheerajkesavan@gmail.com>
2024-11-11 21:44:01 +0530

    day40

--- Day-40/day40.java
+++ Day-40/day40.java
@@ -1,11 +1,12 @@
 //Day40/100: LeetCode: 2481. Minimum Cuts to Divide a Circle
 public class day40 {
-    public int numberOfCuts(int n) {
+    public static int numberOfCuts(int n) {
         if(n==1) return 0;
         if(n % 2 == 0) return n/2;
         return n;
     }
     public static void main(String[] args) {
         int n = 3;
+        int ans = 
     }
 }
