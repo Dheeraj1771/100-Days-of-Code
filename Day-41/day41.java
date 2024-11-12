7254ea87583823779ca1f4983f2bf43110dbaca2
Dheeraj1771 <dheerajkesavan@gmail.com>
2024-11-12 20:09:39 +0530

    day41

--- Day-41/day41.java
+++ Day-41/day41.java
@@ -1,5 +1,13 @@
 
 
 public class day41 {
-    
+    public int largestPerimeter(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=nums.length-1; i>1; i--) {
+            if(nums[i] < nums[i-1] + nums[i-2]) {
+                return nums[i] + nums[i-1] + nums[i-2];
+            }
+        }
+        return 0;
+    }
 }
