81e6581746b615fd5621eb54a09c5291e1d6e92a
Dheeraj1771 <dheerajkesavan@gmail.com>
2024-11-13 18:39:26 +0530

    day42

--- Day-42/day42.java
+++ Day-42/day42.java
@@ -1,6 +1,6 @@
 //Day42/100: LeetCode: 1720. Decode XORed Array
 public class day42 {
-    public int[] decode(int[] encoded, int first) {
+    public static int[] decode(int[] encoded, int first) {
         int decoded[] = new int[encoded.length+1];
         decoded[0] = first;
         for(int i=0; i<encoded.length; i++) {
@@ -11,5 +11,6 @@ public class day42 {
     public static void main(String[] args) {
         int encoded[] = new int[] {1,2,3};
         int first = 1;
+
     }
 }
