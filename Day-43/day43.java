c142b6730f34deac9625467cffde45bc31b46249
Dheeraj1771 <dheerajkesavan@gmail.com>
2024-11-14 23:29:02 +0530

    day43

--- Day-43/day43.java
+++ Day-43/day43.java
@@ -1,4 +1,12 @@
 //Day42/100: LeetCode: 1720. Decode XORed Array
 public class day43 {
-    
+    public int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=0; i<nums.length; i++) {
+            if(i != nums[i]) {
+                return i;
+            }
+        }
+        return nums[nums.length-1] + 1;
+    }
 }
