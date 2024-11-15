931e0a8fed0f7c5b1366678d3f29d032d928dbeb
Dheeraj1771 <dheerajkesavan@gmail.com>
2024-11-15 11:11:11 +0530

    day44

--- Day-44/day44.java
+++ Day-44/day44.java
@@ -1,6 +1,6 @@
 //Day44/100: LeetCode: 1470. Shuffle the Array
 public class day44 {
-    public int[] shuffle(int[] nums, int n) {
+    public static int[] shuffle(int[] nums, int n) {
         int suffled[] = new int[nums.length];
         int k = 0;
         for(int i=0; i<nums.length/2; i++) {
@@ -13,6 +13,7 @@ public class day44 {
     public static void main(String[] args) {
         int n = 3;
         int nums[] = new int[] {2,5,1,3,4,7};
-        
+        int ans[] = shuffle(nums, n);
+
     }
 }
