ff0e47281d95d2a276cffb4a8e9bef6e5ee8e962
Dheeraj1771 <dheerajkesavan@gmail.com>
2024-11-15 11:08:36 +0530

    day44

---
+++ Day-44/day44.java
@@ -0,0 +1,12 @@
+public class day44 {
+    public int[] shuffle(int[] nums, int n) {
+        int suffled[] = new int[nums.length];
+        int k = 0;
+        for(int i=0; i<nums.length/2; i++) {
+            suffled[k] = nums[i];
+            suffled[k+1] = nums[n+i];
+            k=k+2;
+        }
+        return suffled;
+    }
+}
