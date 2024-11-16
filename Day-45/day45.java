d2b8e813b0e73ddee2ac7eb2a20478d87ab1f5e3
Dheeraj1771 <dheerajkesavan@gmail.com>
2024-11-16 21:58:19 +0530

    Day45

---
+++ Day-45/day45.java
@@ -0,0 +1,15 @@
+public class day45 {
+    public int findTheDistanceValue(int[] arr1, int[] arr2, int d) {
+        int count = arr1.length;
+        for(int i=0; i<arr1.length; i++) {
+            for(int j =0; j<arr2.length; j++) {
+                int sub = arr1[i] - arr2[j];
+                if(Math.abs(sub) <= d) {
+                    count--;
+                    break;
+                }
+            }
+        }
+        return count;
+    }
+}
