4218136afd554d0add33f6bc73b3b2045efb21b6
Dheeraj1771 <dheerajkesavan@gmail.com>
2024-11-17 11:44:32 +0530

    day46

--- Day-46/day46.java
+++ Day-46/day46.java
@@ -27,4 +27,9 @@ public class day46 {
         }
         return res;
     }
+    public static void main(String[] args) {
+        int arr1[] = new int[] {2,3,1,3,2,4,6,7,9,2,19};
+        int arr2[] = new int[] {2,1,4,3,9,6};
+        int ans = 
+    }
 }
