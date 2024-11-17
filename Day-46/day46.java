a9b06a9e9b7bbc4b68c4cd804a74c9f35e217a3f
Dheeraj1771 <dheerajkesavan@gmail.com>
2024-11-17 11:46:01 +0530

    day46

--- Day-46/day46.java
+++ Day-46/day46.java
@@ -31,9 +31,10 @@ public class day46 {
         int arr1[] = new int[] {2,3,1,3,2,4,6,7,9,2,19};
         int arr2[] = new int[] {2,1,4,3,9,6};
         int ans[] = relativeSortArray(arr1, arr2);
+        System.out.print("[ ");
         for(int i=0; i<ans.length; i++) {
-            
+            System.out.print(ans[i] + " ");
         }
-
+        System.out.println("]");
     }
 }
