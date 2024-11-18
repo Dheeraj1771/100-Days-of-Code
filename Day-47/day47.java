a983369f773c26a025cac115c99c01625d835cd9
Dheeraj1771 <dheerajkesavan@gmail.com>
2024-11-18 11:38:09 +0530

    day47

---
+++ Day-47/day47.java
@@ -0,0 +1,18 @@
+public class day47 {
+    public int thirdMax(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int element : nums) {
+            set.add(element);
+        }
+        int arr[] = new int[set.size()];
+        int i = 0;
+        for(int ele : set){ 
+          arr[i++] = ele; 
+        } 
+        Arrays.sort(arr);
+        if(arr.length == 1) return arr[0];
+        if(arr.length == 2) return Math.max(arr[0], arr[1]);
+        int ind = arr.length;
+        return arr[ind - 3];
+    }
+}
