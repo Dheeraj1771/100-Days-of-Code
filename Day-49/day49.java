5787ed106a179aaf7107f8d70893f487d8bdcd48
Dheeraj1771 <dheerajkesavan@gmail.com>
2024-11-20 09:04:09 +0530

    Day49

---
+++ Day-49/day49.java
@@ -0,0 +1,11 @@
+public class day49 {
+    public int addedInteger(int[] nums1, int[] nums2) {
+        int max1 = Integer.MIN_VALUE;
+        int max2 = Integer.MIN_VALUE;
+        for(int i=0; i<nums1.length; i++) {
+            max1 = Math.max(max1, nums1[i]);
+            max2 = Math.max(max2, nums2[i]);
+        }
+        return max2 - max1;
+    }
+}
