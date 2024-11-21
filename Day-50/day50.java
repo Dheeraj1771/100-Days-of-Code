fbc9ab4e1096ea70e8c08405928856b08dd2a4ce
Dheeraj1771 <dheerajkesavan@gmail.com>
2024-11-21 21:53:07 +0530

    day50

--- Day-50/day50.java
+++ Day-50/day50.java
@@ -1,12 +1,13 @@
 //Day50/100: LeetCode: 1480. Running Sum of 1d Array
 public class day50 {
-    public int[] runningSum(int[] nums) {
+    public static int[] runningSum(int[] nums) {
         for(int i=1; i<nums.length; i++) {
             nums[i] = nums[i] + nums[i-1];
         }
         return nums;
     }
     public static void main(String[] args) {
-        
+        int nums[] = new int[] {1,2,3,4};
+        int ans[] = 
     }
 }
