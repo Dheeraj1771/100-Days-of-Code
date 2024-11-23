1d4a5adb1b79e54553b96c4b57cf9c9dc53c55b9
Dheeraj1771 <dheerajkesavan@gmail.com>
2024-11-23 19:17:28 +0530

    day52

--- Day-52/day52.java
+++ Day-52/day52.java
@@ -1,3 +1,15 @@
 public class day52 {
-    
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int curr = 0;
+        int max = 0;
+        for(int i=0; i<nums.length; i++) {
+            if(nums[i] == 1) {
+                curr += 1;
+            } else {
+                curr = 0;
+            }
+            max = Math.max(curr, max);
+        }
+        return max;
+    }
 }
