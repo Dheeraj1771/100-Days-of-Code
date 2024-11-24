45fd02a92ce5abb2a6d12639fca3df44e218ea13
Dheeraj1771 <dheerajkesavan@gmail.com>
2024-11-24 21:31:33 +0530

    day53

--- Day-53/day53.java
+++ Day-53/day53.java
@@ -1,6 +1,6 @@
 //Day53/100: LeetCode: 509. Fibonacci Number
 public class day53 {
-    public int fib(int n) {
+    public static int fib(int n) {
         if (n <= 1) return n;
         int a = 0, b = 1;
          for (int i = 2; i <= n; i++) {
@@ -9,5 +9,9 @@ public class day53 {
             a = temp;
         }
         return b;
+    }
+    public static void main(String[] args) {
+        int n = 3;
+        int ans = fib
     }
 }
