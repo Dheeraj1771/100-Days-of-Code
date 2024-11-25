b95d8935d3f24bddb960e992113c6ea630c7e5ce
Dheeraj1771 <dheerajkesavan@gmail.com>
2024-11-25 21:49:38 +0530

    day54

--- Day-54/day54.java
+++ Day-54/day54.java
@@ -1,3 +1,17 @@
 public class day54 {
-    
+    public int findLucky(int[] arr) {
+        int lucky_int = -1;
+        int freq[] = new int[arr.length + 1];
+        for(int i : arr) {
+            if(i >= 1 && i <= arr.length) {
+                freq[i]++;
+            }
+        }
+        for(int i=1; i<=arr.length; i++) {
+            if(freq[i] == i) {
+                lucky_int = Math.max(i, lucky_int);
+            }
+        }
+        return lucky_int;
+    }
 }
