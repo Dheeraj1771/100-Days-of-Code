736ddda2311b11bddc0d4966a8e76c0b9afd27b6
Dheeraj1771 <dheerajkesavan@gmail.com>
2024-11-26 22:43:55 +0530

    day55

--- Day-55/day55.java
+++ Day-55/day55.java
@@ -1,3 +1,14 @@
 public class day55 {
-    
+    public boolean isPalindrome(String s) {
+        String cleanStr = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        StringBuilder n = new StringBuilder();
+        for(int i=cleanStr.length()-1; i>=0; i--) {
+            n.append(cleanStr.charAt(i));
+        }
+        if(cleanStr.equals(n.toString())) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
