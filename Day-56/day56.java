ddd2d468f071b3b8b702494e0f5f6a5fc73791ac
Dheeraj1771 <dheerajkesavan@gmail.com>
2024-11-27 11:14:37 +0530

    Day56

--- Day-56/day56.java
+++ Day-56/day56.java
@@ -4,7 +4,6 @@ public class day56 {
         for(int i=0; i<nums.length; i++) {
             int count = 0;
             while(nums[i] > 0) {
-                int digit = nums[i] % 10;
                 count++;
                 nums[i] /= 10;
             }
