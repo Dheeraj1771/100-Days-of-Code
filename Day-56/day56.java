a90108e51d05013e0f627ae0da18986f545d6e34
Dheeraj1771 <dheerajkesavan@gmail.com>
2024-11-27 11:10:21 +0530

    Day56

--- Day-56/day56.java
+++ Day-56/day56.java
@@ -1,3 +1,17 @@
 public class day56 {
-    
+    public int findNumbers(int[] nums) {
+        int res = 0;
+        for(int i=0; i<nums.length; i++) {
+            int count = 0;
+            while(nums[i] > 0) {
+                int digit = nums[i] % 10;
+                count++;
+                nums[i] /= 10;
+            }
+            if(count % 2 == 0) {
+                res++;
+            }
+        }
+        return res;
+    }
 }
