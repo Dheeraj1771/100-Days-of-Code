1cf54f9feb1ec8895ad21dfe7be08f7b0758887c
Dheeraj1771 <dheerajkesavan@gmail.com>
2024-11-28 09:28:40 +0530

    day57

--- Day-57/day57.java
+++ Day-57/day57.java
@@ -1,3 +1,23 @@
 public class day57 {
-    
+    public boolean detectCapitalUse(String word) {
+        int lower = 0;
+        int upper = 0;
+        for(int i=0; i<word.length(); i++) {
+            if(Character.isUpperCase(word.charAt(i))) {
+                upper++;
+            } 
+            else {
+                lower++;
+            } 
+        }
+        if(upper == word.length() || lower == word.length()){
+            return true;
+        } 
+        else if(Character.isUpperCase(word.charAt(0)) && lower == word.length()-1){
+            return true;
+        }   
+        else {
+            return false;
+        }
+    }
 }
