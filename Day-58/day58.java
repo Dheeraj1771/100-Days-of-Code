cf98f73fef5a5505a1a033f4da6d1ca12609611c
Dheeraj1771 <dheerajkesavan@gmail.com>
2024-11-29 11:42:45 +0530

    day58

--- Day-58/day58.java
+++ Day-58/day58.java
@@ -1,3 +1,10 @@
 public class day58 {
-    
+    public String truncateSentence(String s, int k) {
+        String arr[] = s.split(" ");
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<k; i++) {
+            sb.append(arr[i] + " ");
+        }
+        return sb.toString().trim();
+    }
 }
