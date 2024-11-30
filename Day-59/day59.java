6f4e47ef4464a595447b848ad478064511d0627c
Dheeraj1771 <dheerajkesavan@gmail.com>
2024-11-30 22:32:23 +0530

    day59

--- Day-59/day59.java
+++ Day-59/day59.java
@@ -1,4 +1,16 @@
 //Day59/100: LeetCode: 3340. Check Balanced String
 public class day59 {
-    
+    public boolean isBalanced(String num) {
+        int odd = 0;
+        int even = 0;
+        char arr[] = num.toCharArray();
+        for(int i=0; i<arr.length; i+=2) {
+            even += Character.getNumericValue(arr[i]);
+        }
+        for(int j=1; j<arr.length; j+=2) {
+            odd += Character.getNumericValue(arr[j]); 
+        }
+        if(odd == even) return true;
+        else return false;
+    }
 }
