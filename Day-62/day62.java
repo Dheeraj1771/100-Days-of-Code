8104fba6ec56705cc26db388eb21c551385fc082
Dheeraj1771 <dheerajkesavan@gmail.com>
2024-12-03 10:04:30 +0530

    day62

--- Day-62/day62.java
+++ Day-62/day62.java
@@ -1,5 +1,5 @@
 public class day62 {
-    public boolean checkIfExist(int[] arr) {
+    public static boolean checkIfExist(int[] arr) {
         for(int i=0; i<arr.length; i++) {
             for(int j=0; j<arr.length; j++) {
                 if(arr[i] == 2 * arr[j]) {
@@ -8,5 +8,10 @@ public class day62 {
             }
         }
         return false;
+    }
+    public static void main(String[] args) {
+        int arr[] = new int[] {7,1,14,11};
+        boolean ans = checkIfExist(arr);
+        System.out.println(ans);
     }
 }
