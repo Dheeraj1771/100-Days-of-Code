00ab34f27583714d587f840a2a88519160b87429
Dheeraj1771 <dheerajkesavan@gmail.com>
2024-12-04 10:14:43 +0530

    day63

--- Day-63/day63.java
+++ Day-63/day63.java
@@ -1,19 +1,21 @@
 //Day63/100: LeetCode 344. Reverse String
-public static void reverseString(char[] s) {
-    int j = s.length - 1;
-    for (int i = 0; i < s.length / 2; i++) {
-        char k = s[i];
-        s[i] = s[j];
-        s[j] = k;
-        j--;
+public class day63 {
+    public static void reverseString(char[] s) {
+        int j = s.length - 1;
+        for (int i = 0; i < s.length / 2; i++) {
+            char k = s[i];
+            s[i] = s[j];
+            s[j] = k;
+            j--;
+        }
+        System.out.print("[ ");
+        for (int i = 0; i < s.length; i++) {
+            System.out.print(s[i] + " ");
+        }
+        System.out.print("]");
     }
-    System.out.print("[ ");
-    for (int i = 0; i < s.length; i++) {
-        System.out.print(s[i] + " ");
+    public static void main(String[] args) {
+        char s[] = new char[] { 'h', 'e', 'l', 'l', 'o' };
+        reverseString(s);
     }
-    System.out.print("]");
-}
-public static void main(String[] args) {
-    char s[] = new char[] { 'h', 'e', 'l', 'l', 'o' };
-    reverseString(s);
 }
