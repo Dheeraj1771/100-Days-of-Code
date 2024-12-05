2ebd116d9de3f07b8e26f35626c4e572f6369ae5
Dheeraj1771 <dheerajkesavan@gmail.com>
2024-12-05 09:35:40 +0530

    day64

--- Day-64/day64.java
+++ Day-64/day64.java
@@ -1,3 +1,4 @@
+//Day64/100: LeetCode 1528. Shuffle String
 public class day64 {
     public String restoreString(String s, int[] indices) {
         char sarr[] = new char[s.length()];
@@ -7,5 +8,9 @@ public class day64 {
             j++;
         }
         return new String(sarr);
+    }
+    public static void main(String[] args) {
+        String s = "codeleet";
+        
     }
 }
