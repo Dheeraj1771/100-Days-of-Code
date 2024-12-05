98713e7f1cccc1f0938b28c4f7d0399b8ccf32fa
Dheeraj1771 <dheerajkesavan@gmail.com>
2024-12-05 09:34:38 +0530

    day64

--- Day-64/day64.java
+++ Day-64/day64.java
@@ -1,3 +1,11 @@
 public class day64 {
-    
+    public String restoreString(String s, int[] indices) {
+        char sarr[] = new char[s.length()];
+        int j = 0;
+        for(int i : indices) {
+            sarr[i] = s.charAt(j);
+            j++;
+        }
+        return new String(sarr);
+    }
 }
