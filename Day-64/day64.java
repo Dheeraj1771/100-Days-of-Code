951d257bf26a32a43091ffd0339b0cfeb6433ee4
Dheeraj1771 <dheerajkesavan@gmail.com>
2024-12-05 09:36:38 +0530

    day64

--- Day-64/day64.java
+++ Day-64/day64.java
@@ -12,6 +12,7 @@ public class day64 {
     public static void main(String[] args) {
         String s = "codeleet";
         int indices[] = new int[] {4,5,6,7,0,2,1,3};
-
+        String ans = restoreString(s, indices);
+        System.out.println(ans);
     }
 }
