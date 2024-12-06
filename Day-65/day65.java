2958684e474891b71716fc1732b2efdc9a44bac6
Dheeraj1771 <dheerajkesavan@gmail.com>
2024-12-06 09:42:36 +0530

    day65

---
+++ Day-65/day65.java
@@ -0,0 +1,14 @@
+public class day65 {
+    public static boolean rotateString(String s, String goal) {
+        if(s.length() != goal.length()) {
+            return false;
+        }
+        return (s+s).contains(goal);
+    }
+    public static void main(String[] args) {
+        String s = "abcde";
+        String goal = "cdeab";
+        boolean ans = rotateString(s, goal);
+        System.out.println(ans);
+    }
+}
