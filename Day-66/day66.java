2c33f8aca0b6f0c860154a36bf08762472f87811
Dheeraj1771 <dheerajkesavan@gmail.com>
2024-12-07 09:53:37 +0530

    day66

--- Day-66/day66.java
+++ Day-66/day66.java
@@ -1,3 +1,13 @@
 public class day66 {
-    
+    public int findPermutationDifference(String s, String t) {
+        int perdiff = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        for(int i=0; i<t.length(); i++) {
+            map.put(t.charAt(i), i);
+        }
+        for(int i=0; i<s.length(); i++) {
+            perdiff += Math.abs(i - map.get(s.charAt(i)));
+        }
+        return perdiff;
+    }
 }
