7f1d57d3cf2dea63e666db75d7c833288173bcbb
Dheeraj1771 <dheerajkesavan@gmail.com>
2024-12-08 08:36:12 +0530

    day67

--- Day-67/day67.java
+++ Day-67/day67.java
@@ -1,9 +1,13 @@
 public class day67 {
-    public int scoreOfString(String s) {
+    public static int scoreOfString(String s) {
         int score = 0;
         for(int i=0; i<s.length()-1; i++) {
             score += Math.abs((s.charAt(i)-'a') - (s.charAt(i+1) - 'a'));
         }
         return score;
+    }
+    public static void main(String[] args) {
+        String s = "hello";
+        
     }
 }
