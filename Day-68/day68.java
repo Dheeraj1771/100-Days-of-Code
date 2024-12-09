cd46b8c551d50e2c9a60fc764ec3cb112383bb7f
Dheeraj1771 <dheerajkesavan@gmail.com>
2024-12-09 09:36:45 +0530

    day68

--- Day-68/day68.java
+++ Day-68/day68.java
@@ -1,3 +1,16 @@
 public class day68 {
-    
+    public String firstPalindrome(String[] words) {
+        for(int i=0; i<words.length; i++) {
+            StringBuilder sb = new StringBuilder();
+            String s = words[i];
+            for(int j=s.length()-1; j>=0; j--) {
+                sb.append(s.charAt(j));
+            }
+            if(sb.toString().equals(words[i])) {
+                return sb.toString();
+            }
+            
+        }
+        return "";
+    }
 }
