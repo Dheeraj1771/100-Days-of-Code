c87834a57c896a0a9e362e1b4c27373ab95bbe71
Dheeraj1771 <dheerajkesavan@gmail.com>
2024-12-10 09:10:22 +0530

    day69

--- Day-69/day69.java
+++ Day-69/day69.java
@@ -1,3 +1,13 @@
 public class day69 {
-    
+    public String defangIPaddr(String address) {
+        StringBuilder sb = new StringBuilder();
+        for(char c : address.toCharArray()) {
+            if(c == '.') {
+                sb.append("[.]");
+            } else {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
 }
