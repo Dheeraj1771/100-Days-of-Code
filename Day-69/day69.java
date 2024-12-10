e304cb70b9fbbf68972985f65d1132abe5355e5b
Dheeraj1771 <dheerajkesavan@gmail.com>
2024-12-10 09:12:25 +0530

    day69

--- Day-69/day69.java
+++ Day-69/day69.java
@@ -14,5 +14,6 @@ public class day69 {
     public static void main(String[] args) {
         String address = "1.1.1.1";
         String ans = defangIPaddr(address);
+        System.out.println(ans);
     }
 }
