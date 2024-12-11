76ab99f363845537f510b8a468b0d6c9530ef512
Dheeraj1771 <dheerajkesavan@gmail.com>
2024-12-11 07:21:22 +0530

    day70

--- Day-70/day70.java
+++ Day-70/day70.java
@@ -1,3 +1,15 @@
 public class day70 {
-    
+    public int numJewelsInStones(String jewels, String stones) {
+        char arr1[] = jewels.toCharArray();
+        char arr2[] = stones.toCharArray();
+        int count = 0;
+        for(char c1 : arr1) {
+            for(char c2 : arr2) {
+                if(c1 == c2) {
+                    count += 1;
+                }
+            }
+        }
+        return count;
+    }
 }
