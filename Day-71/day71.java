aae7cb68a489507da698e28ec396e1bb566b9a00
Dheeraj1771 <dheerajkesavan@gmail.com>
2024-12-12 21:29:45 +0530

    day71

---
+++ Day-71/day71.java
@@ -0,0 +1,18 @@
+public class day71 {
+    public static char findTheDifference(String s, String t) {
+        char result = 0;
+        for(int i = 0; i<s.length(); i++){
+            result = (char)(result ^ s.charAt(i));
+        }
+        for(int i = 0; i<t.length(); i++){
+            result = (char)(result ^ t.charAt(i));
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        String s = "abcd";
+        String t = "abcde";
+        char ans = findTheDifference(s, t);
+        System.out.println(ans);
+    }
+}
