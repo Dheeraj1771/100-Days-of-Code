91e32c15e0c09d86c25708a8b04be4845c98f31f
Dheeraj1771 <dheerajkesavan@gmail.com>
2024-12-13 21:50:36 +0530

    day72

--- Day-72/day72.java
+++ Day-72/day72.java
@@ -1,3 +1,13 @@
 public class day72 {
-    
+    public int countSegments(String s) {
+        int count = 0;
+        String arr[] = s.split(" ");
+        for(String c : arr) {
+            if(!c.isEmpty()) {
+                count += 1;
+            }
+        }
+        return count;
+
+    }
 }
