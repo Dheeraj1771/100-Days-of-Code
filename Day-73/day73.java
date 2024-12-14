8b1e64b3c9ba37b77a3018f87790458560a2c318
Dheeraj1771 <dheerajkesavan@gmail.com>
2024-12-14 22:11:15 +0530

    day73

---
+++ Day-73/day73.java
@@ -0,0 +1,21 @@
+//Day73/100: LeetCode 657. Robot Return to Origin
+public class day73 {
+    public boolean judgeCircle(String moves) {
+        int up = 0;
+        int down = 0;
+        int left = 0;
+        int right = 0;
+        for(int i=0; i<moves.length(); i++) {
+            if(moves.charAt(i) == 'R') {
+                right++;
+            } else if(moves.charAt(i) == 'L') {
+                left++;
+            } else if(moves.charAt(i) == 'U') {
+                up++;
+            } else {
+                down++;
+            }
+        }
+        return up == down && left == right;
+    }
+}
