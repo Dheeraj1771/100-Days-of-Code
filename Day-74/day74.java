fcca9601a86e1c13ad8f5220f1134423662c703a
Dheeraj1771 <dheerajkesavan@gmail.com>
2024-12-15 19:34:31 +0530

    day74

--- Day-74/day74.java
+++ Day-74/day74.java
@@ -1,3 +1,10 @@
 public class day74 {
-    
+    public String toLowerCase(String s) {
+        char res[] = s.toCharArray();
+        for(int i=0; i<s.length(); i++) {
+            res[i] = Character.toLowerCase(res[i]);
+        }
+        String a = new String(res);
+        return a;
+    }
 }
