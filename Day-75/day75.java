3cf40dead84e092349cd9d23bacf8cd2635dcb01
Dheeraj1771 <dheerajkesavan@gmail.com>
2024-12-16 16:52:47 +0530

    day75

--- Day-75/day75.java
+++ Day-75/day75.java
@@ -1,3 +1,29 @@
 public class day75 {
-    
+    public boolean buddyStrings(String s, String goal) {
+        if(s.length() != goal.length()) return false;
+        if(s.equals(goal)) {
+            Set<Character> unique_chars = new HashSet<>();
+            for(char c : s.toCharArray()) {
+                unique_chars.add(c);
+            }
+            if(unique_chars.size() < s.length()) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        List<Integer> diff = new ArrayList<>();
+        for(int i=0; i<s.length(); i++) {
+            if(s.charAt(i) != goal.charAt(i)) {
+                diff.add(i);
+            }
+        }
+        if(diff.size() == 2 &&
+        s.charAt(diff.get(0)) == goal.charAt(diff.get(1)) && 
+        s.charAt(diff.get(1)) == goal.charAt(diff.get(0))) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
