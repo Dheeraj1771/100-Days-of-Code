bf75b9711f8fa870f79d4319413c95d94e4c399d
Dheeraj1771 <dheerajkesavan@gmail.com>
2024-12-17 07:43:50 +0530

    day76

--- Day-76/day76.java
+++ Day-76/day76.java
@@ -1,3 +1,14 @@
 public class day76 {
-    
+    public List<Integer> findWordsContaining(String[] words, char x) {
+        ArrayList<Integer> arrlst = new ArrayList<>();
+        for(int i=0; i<words.length; i++) {
+            for(int j=0; j<words[i].length(); j++) {
+                if(words[i].charAt(j) == x) {
+                    arrlst.add(i);
+                    break;
+                }
+            }
+        }
+        return arrlst;
+    }
 }
