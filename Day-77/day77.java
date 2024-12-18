d0780866242a039c54778d9fc80fc9cf5204f9b6
Dheeraj1771 <dheerajkesavan@gmail.com>
2024-12-18 09:24:30 +0530

    day77

--- Day-77/day77.java
+++ Day-77/day77.java
@@ -1,3 +1,14 @@
+//Day77/100: LeetCode 2011. Final Value of Variable After Performing Operations
 public class day77 {
-    
+    public int finalValueAfterOperations(String[] operations) {
+        int X = 0;
+        for(String s : operations) {
+            if(s.charAt(1) == '+') {
+                X++;
+            } else {
+                X--;
+            }
+        }
+        return X;
+    }
 }
