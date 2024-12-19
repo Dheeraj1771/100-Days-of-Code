46321b744a4af19f79a6db6b7ee28449119a0c9d
Dheeraj1771 <dheerajkesavan@gmail.com>
2024-12-19 09:59:07 +0530

    day78

--- Day-78/day78.java
+++ Day-78/day78.java
@@ -1,4 +1,22 @@
 public class day78 {
+    public String interpret(String command) {
+        String res = "";
+        for(int i=0; i<command.length(); i++) {
+            if(command.charAt(i) == 'G') {
+                res += "G";
+            } 
+            if(command.charAt(i) == '(') {
+                if(command.charAt(i+1) == ')') {
+                    res += "o";
+                    i++;
+                } else {
+                    res += "al";
+                    i+=3;
+                }
+            }
+        }
+        return res;
+    }
     public static void main(String[] args) {
         String ans =
     }
