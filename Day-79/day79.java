ccbf77646c3e688b6b8977d41106292e4adcee76
Dheeraj1771 <dheerajkesavan@gmail.com>
2024-12-20 09:20:11 +0530

    day79

--- Day-79/day79.java
+++ Day-79/day79.java
@@ -1,3 +1,10 @@
 public class day79 {
-    
+    public int mostWordsFound(String[] sentences) {
+        int maxlen = 0;
+        for(String s : sentences) {
+            int currlen = s.split(" ").length;
+            maxlen = Math.max(maxlen, currlen);
+        }
+        return maxlen;
+    }
 }
