eaf602a2f4584df9a74efab6644428071bf7a501
Dheeraj1771 <dheerajkesavan@gmail.com>
2024-12-21 10:24:48 +0530

    day80

--- Day-80/day80.java
+++ Day-80/day80.java
@@ -1,6 +1,7 @@
+//Day80/100: LeetCode 412. Fizz Buzz
 import java.util.*;
 public class day80 {
-    public List<String> fizzBuzz(int n) {
+    public static List<String> fizzBuzz(int n) {
         ArrayList<String> list = new ArrayList<>();
         for(int i=1; i<=n; i++) {
             if(i%3 == 0 && i%5 == 0) {
@@ -14,5 +15,8 @@ public class day80 {
             }
         }
         return list;
+    }
+    public static void main(String[] args) {
+        int n = 6;
     }
 }
