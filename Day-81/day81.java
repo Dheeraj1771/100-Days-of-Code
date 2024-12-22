a640a78bb7d9b00b801ac272be2935946fc3833e
Dheeraj1771 <dheerajkesavan@gmail.com>
2024-12-22 09:57:17 +0530

    day81

--- Day-81/day81.java
+++ Day-81/day81.java
@@ -1,3 +1,17 @@
 public class day81 {
-    
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        String w1 = "";
+        String w2 = "";
+        for(int i=0; i<word1.length; i++) {
+            w1 += word1[i];
+        }
+        for(int j=0; j<word2.length; j++) {
+            w2 += word2[j];
+        }
+        if(w1.equals(w2)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
