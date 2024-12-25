e4881b42ebcdb7e21cf586ab564c6f01f6c04b11
Dheeraj1771 <dheerajkesavan@gmail.com>
2024-12-25 10:17:34 +0530

    day84

--- Day-84/day84.java
+++ Day-84/day84.java
@@ -1,3 +1,16 @@
 public class day84 {
-    
+    public int[] getSneakyNumbers(int[] nums) {
+        int arr[] = new int[2];
+        int j = 0;
+        HashSet<Integer> set = new HashSet<>();
+        for(int i : nums) {
+            if(set.contains(i)) {
+                arr[j] = i;
+                j++;
+            } else {
+                set.add(i);
+            }
+        }
+        return arr;
+    }
 }
