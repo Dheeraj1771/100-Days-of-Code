a253e6938805a919b6fd64d84518e49ef3dd6a2f
Dheeraj1771 <dheerajkesavan@gmail.com>
2024-12-26 07:54:32 +0530

    day85

--- Day-85/day85.java
+++ Day-85/day85.java
@@ -1,3 +1,11 @@
 public class day85 {
-    
+    public int minimumOperations(int[] nums) {
+        int op = 0;
+        for(int i: nums) {
+            if(i%3 == 1 || i%3 == 2) {
+                op++;
+            }
+        }
+        return op;
+    }
 }
