f56d1ba15ce1faa37758981f7ed7168662699846
Dheeraj1771 <dheerajkesavan@gmail.com>
2024-12-27 09:44:51 +0530

    day86

--- Day-86/day86.java
+++ Day-86/day86.java
@@ -1,3 +1,13 @@
 public class day86 {
-    
+    public int maximumWealth(int[][] accounts) {
+        int maxwealth = 0;
+        for(int i[] : accounts) {
+            int currwealth = 0;
+            for(int j=0; j<i.length; j++) {
+                currwealth += i[j];
+                maxwealth = Math.max(currwealth, maxwealth);
+            }
+        }
+        return maxwealth;
+    }
 }
