8e54e19b10a63f5a0a8df3962dfe8e859a9a2fe8
Dheeraj1771 <dheerajkesavan@gmail.com>
2024-12-28 16:42:45 +0530

    day87

--- Day-87/day87.java
+++ Day-87/day87.java
@@ -1,3 +1,19 @@
 public class day87 {
-    
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        ArrayList<Boolean> list = new ArrayList<>();
+        int maxcan = 0;
+        int currcan = 0;
+        for(int i=0; i<candies.length; i++) {
+            currcan = candies[i];
+            maxcan = Math.max(currcan, maxcan);
+        }
+        for(int j : candies) {
+            if(j + extraCandies >= maxcan) {
+                list.add(true);
+            } else {
+                list.add(false);
+            }
+        }
+        return list;
+    }
 }
