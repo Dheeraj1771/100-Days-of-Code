64ae9c5b7e696f10735551fd1257a420f0eabc48
Dheeraj1771 <dheerajkesavan@gmail.com>
2024-12-29 11:12:47 +0530

    day88

--- Day-88/day88.java
+++ Day-88/day88.java
@@ -1,3 +1,21 @@
 public class day88 {
-    
+    public String reverseOnlyLetters(String s) {
+        String letters = "";
+        for(int i = s.length()-1; i>=0; i--) {
+            if(Character.isLetter(s.charAt(i))) {
+                letters += s.charAt(i);
+            }
+        }
+        String result = "";
+        int index = 0;
+        for(int i=0; i<s.length(); i++) {
+            if(Character.isLetter(s.charAt(i))) {
+                result += letters.charAt(index);
+                index++;
+            } else {
+                result += s.charAt(i);
+            }
+        }
+        return result;
+    }
 }
