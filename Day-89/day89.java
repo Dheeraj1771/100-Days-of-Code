d8d227df6ddec37c0bfe0102967c32ad83e42ec5
Dheeraj1771 <dheerajkesavan@gmail.com>
2024-12-30 09:49:26 +0530

    day89

--- Day-89/day89.java
+++ Day-89/day89.java
@@ -1,3 +1,4 @@
+//Day89/100: LeetCode 2798. Number of Employees Who Met the Target
 public class day89 {
     public int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
         int count = 0;
