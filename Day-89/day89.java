9dacee2850f3085dc97c56d9fd770bfde1a28636
Dheeraj1771 <dheerajkesavan@gmail.com>
2024-12-30 09:48:59 +0530

    day89

--- Day-89/day89.java
+++ Day-89/day89.java
@@ -1,3 +1,11 @@
 public class day89 {
-    
+    public int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
+        int count = 0;
+        for(int i : hours) {
+            if(i >= target) {
+                count++;
+            }
+        }
+        return count;
+    }
 }
