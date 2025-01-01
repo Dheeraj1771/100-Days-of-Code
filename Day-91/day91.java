0fbafe48f554a29d5fb5193693c5d03c470f5326
Dheeraj1771 <dheerajkesavan@gmail.com>
2025-01-01 10:37:50 +0530

    day91

--- Day-91/day91.java
+++ Day-91/day91.java
@@ -1,6 +1,6 @@
 //Day91/100: LeetCode 3264. Final Array State After K Multiplication Operations I
 public class day91 {
-    public int[] getFinalState(int[] nums, int k, int multiplier) {
+    public static int[] getFinalState(int[] nums, int k, int multiplier) {
         for(int i=0; i<k; i++) {
             int min = nums[0];
             int x = 0;
@@ -18,5 +18,6 @@ public class day91 {
         int nums[] = new int[] {2,1,3,5,6};
         int k = 5;
         int multiplier = 2;
+        int ans[] = getFinalState(nums, k, multiplier);
     }
 }
