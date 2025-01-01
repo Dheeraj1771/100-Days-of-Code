7b15ccae06fb3c66140c960bff4645b53f8c1ff8
Dheeraj1771 <dheerajkesavan@gmail.com>
2025-01-01 10:36:09 +0530

    day91

--- Day-91/day91.java
+++ Day-91/day91.java
@@ -1,3 +1,16 @@
 public class day91 {
-    
+    public int[] getFinalState(int[] nums, int k, int multiplier) {
+        for(int i=0; i<k; i++) {
+            int min = nums[0];
+            int x = 0;
+            for(int j=0; j<nums.length; j++) {
+                if(nums[j] < min) {
+                    min = nums[j];
+                    x = j;
+                } 
+            }
+            nums[x] = min * multiplier;
+        }
+        return nums;
+    }
 }
