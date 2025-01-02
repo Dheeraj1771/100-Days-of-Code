6af13dabf19cd7b2ef2428bc119dac2ea3922157
Dheeraj1771 <dheerajkesavan@gmail.com>
2025-01-02 15:52:02 +0530

    Day92

--- Day-92/day92.java
+++ Day-92/day92.java
@@ -1,6 +1,6 @@
 import java.util.*;
 public class day92 {
-    public int countPairs(List<Integer> nums, int target) {
+    public static int countPairs(List<Integer> nums, int target) {
         int count = 0;
         for(int i=0; i<nums.size()-1; i++) {
             for(int j=i+1; j<nums.size(); j++) {
@@ -10,6 +10,13 @@ public class day92 {
         return count;
     }
     public static void main(String[] args) {
-        
+        List<Integer> nums = new ArrayList<>();
+        nums.add(-1);
+        nums.add(1);
+        nums.add(2);
+        nums.add(3);
+        nums.add(1);
+        int target =  2;
+        int ans = countPairs(nums, target);
     }
 }
