bbd47a953d2d78522e06b2fe02c95a1066198ced
Dheeraj1771 <dheerajkesavan@gmail.com>
2025-01-03 09:39:01 +0530

    day93

--- Day-93/day93.java
+++ Day-93/day93.java
@@ -1,3 +1,11 @@
 public class day93 {
-    
+    public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
+        int count = 0;
+        for(List i : items) {
+            if(ruleKey.equals("type") && i.get(0).equals(ruleValue)) count++;
+            else if(ruleKey.equals("color") && i.get(1).equals(ruleValue)) count++;
+            else if(ruleKey.equals("name") && i.get(2).equals(ruleValue)) count++;
+        }
+        return count;
+    }
 }
