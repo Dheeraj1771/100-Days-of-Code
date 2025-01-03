25eedfb5f855923558db98f871f486a159c4ccc3
Dheeraj1771 <dheerajkesavan@gmail.com>
2025-01-03 09:50:50 +0530

    day93

--- Day-93/day93.java
+++ Day-93/day93.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class day93 {
     public static int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
         int count = 0;
-        for(List i : items) {
+        for(List<String> i : items) {
             if(ruleKey.equals("type") && i.get(0).equals(ruleValue)) count++;
             else if(ruleKey.equals("color") && i.get(1).equals(ruleValue)) count++;
             else if(ruleKey.equals("name") && i.get(2).equals(ruleValue)) count++;
