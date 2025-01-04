79be32d9817c500ae346ed52fbe5a83343df3dd7
Dheeraj1771 <dheerajkesavan@gmail.com>
2025-01-04 17:30:08 +0530

    day94

--- Day-94/day94.java
+++ Day-94/day94.java
@@ -1,3 +1,13 @@
 public class day94 {
-    
+    public String finalString(String s) {
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<s.length(); i++) {
+            if(s.charAt(i) == 'i') {
+                sb.reverse();
+            } else {
+                sb.append(s.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
 }
