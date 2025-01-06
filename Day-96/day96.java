a7bc548232375854fc5b97cb09c6ea8c7ea6bead
Dheeraj1771 <dheerajkesavan@gmail.com>
2025-01-06 09:47:01 +0530

    Day96

--- Day-96/day96.java
+++ Day-96/day96.java
@@ -1,3 +1,11 @@
 public class day96 {
-    
+    public List<Integer> stableMountains(int[] height, int threshold) {
+        List<Integer> list = new ArrayList<>();
+        for(int i=0; i<height.length-1; i++) {
+            if(height[i] > threshold) {
+                list.add(i+1);
+            }
+        }
+        return list;
+    }
 }
