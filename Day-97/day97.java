b0d32a74e50e690d865eeb6b6dddd87b6c681c91
Dheeraj1771 <dheerajkesavan@gmail.com>
2025-01-07 09:28:01 +0530

    day97

--- Day-97/day97.java
+++ Day-97/day97.java
@@ -1,3 +1,20 @@
-public class day97 {
-    
+//Day97/100: LeetCode 2574. Left and Right Sum Differences
+    public int[] leftRightDifference(int[] nums) {
+        int n = nums.length;
+        int answer[] = new int[n];
+        int leftsum[] = new int[n];
+        leftsum[0] = 0;
+        int rightsum[] = new int[n];
+        rightsum[n-1] = 0;
+        for(int i=1; i<n; i++) {
+            leftsum[i] = leftsum[i-1] + nums[i-1];
+        }
+        for(int j=n-2; j>=0; j--) {
+            rightsum[j] = rightsum[j+1] + nums[j+1];
+        }
+        for(int k=0;k<n; k++) {
+            answer[k] = Math.abs(leftsum[k] - rightsum[k]); 
+        }
+        return answer;
+    }
 }
