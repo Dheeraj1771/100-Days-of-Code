3b0a4abe7b1342329b5469f960a3d4015b8163b2
Dheeraj1771 <dheerajkesavan@gmail.com>
2025-01-08 15:41:34 +0530

    day98

--- Day-98/day98.java
+++ Day-98/day98.java
@@ -1,5 +1,7 @@
+//Day98/100: LeetCode 1389. Create Target Array in the Given Order
+import java.util.*;
 public class day98 {
-    public int[] createTargetArray(int[] nums, int[] index) {
+    public static int[] createTargetArray(int[] nums, int[] index) {
         ArrayList<Integer> targetList = new ArrayList<>();
         int target[] = new int[nums.length];
         for(int i=0; i<nums.length; i++) {
@@ -9,5 +11,15 @@ public class day98 {
             target[i] = targetList.get(i);
         }
         return target;
+    }
+    public static void main(String[] args) {
+        int nums[] = new int[] {0,1,2,3,4};
+        int index[] = new int[] {0,1,2,2,1};
+        int ans[] = createTargetArray(nums, index);
+        System.out.print("[ ");
+        for(int i=0; i<ans.length; i++) {
+            System.out.print(ans[i] + " ");
+        }
+        System.out.print("]");
     }
 }
