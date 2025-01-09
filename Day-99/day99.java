7d6a1446e543014005cec976cbcea700154386d9
Dheeraj1771 <dheerajkesavan@gmail.com>
2025-01-09 09:00:45 +0530

    day99

--- Day-99/day99.java
+++ Day-99/day99.java
@@ -1,3 +1,15 @@
+//Day99/100: LeetCode 2469. Convert the Temperature
 public class day99 {
-    
+    public static double[] convertTemperature(double celsius) {
+        double ans[] = new double[2];
+        double kelvin = celsius + 273.15;
+        double fahrenheit = celsius * 1.80 + 32;
+        ans[0] = kelvin;
+        ans[1] = fahrenheit;
+        return ans;
+    }
+    public static void main(String[] args) {
+        double celsius = 36.50;
+
+    }
 }
